dffccc9766b3b2bbfa72d9da3426034c25ffd5f1
Duke Lu <23153285987@qq.com>
2019-02-28 00:13:26 +0800

    User 类添加主键字段

--- src/main/java/com/idukelu/demo/demoeurekaclient/business/pojo/dto/User.java
+++ src/main/java/com/idukelu/demo/demoeurekaclient/business/pojo/dto/User.java
@@ -2,9 +2,19 @@ package com.idukelu.demo.demoeurekaclient.business.pojo.dto;
 
 public class User {
 
+    private String id;
+
     private String username;
 
     private String password;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 
     public String getUsername() {
         return username;
